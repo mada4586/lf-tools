447d59be8daa88b9d8df6dbdaed81aa100d02572
Marcin Dabrowski <vgm@kmd.dk>
2020-02-12 12:10:24 +0100

    Java 8 in ApiServer.

--- run-api-server.cmd
+++ run-api-server.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set JAVA_HOME=%JAVA_7%
+set JAVA_HOME=%JAVA_8%
 
 set PATH=%JAVA_HOME%\bin;%PATH%
 
--- run-mysql.cmd
+++ run-mysql.cmd
@@ -1,1 +1,2 @@
-docker run -d -p 3306:3306 --name mysql_57 -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:5.7.11
+docker run -d -p 3307:3306 --name mysql_57 -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:5.7 --innodb_flush_log_at_trx_commit=2 --sql_mode=
+
