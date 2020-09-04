d34ad7dfcfc065246152eb8c7fd6e7a2e140192c
Marcin Dabrowski <vgm@kmd.dk>
2020-09-04 16:45:56 +0200

    Updated system vars.

--- set-system-vars.cmd
+++ set-system-vars.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-setx DEV_DRIVE c /M
-setx TOOLS %DEV_DRIVE%:\tools /M
-setx SRC %DEV_DRIVE%:\src /M
+setx DEV_DRIVE D: /M
+setx TOOLS %DEV_DRIVE%\tools /M
+setx SRC %DEV_DRIVE%\src /M
 
 setx JAVA_7 %TOOLS%\jdk1.7.0_55 /M
 setx JAVA_8 %TOOLS%\jdk1.8.0_211 /M
@@ -11,18 +11,13 @@ setx JAVA_12 %TOOLS%\jdk-12.0.1 /M
 setx JAVA_13 %TOOLS%\jdk-13 /M
 setx JAVA_HOME %JAVA_7%
 
-:: For API Server
-setx JDK_17 %JAVA_7%
-
 setx NODE_8 %TOOLS%\node-v8.17.0 /M
 setx NODE_8_NPM %TOOLS%\.npm-6.13.4 /M
 setx NODE_10 %TOOLS%\node-v10.16.3 /M
 setx NODE_10_NPM %TOOLS%\.npm-6.9.0 /M
-setx NODE_10_22 %TOOLS%\node-v10.22.0 /M
-setx NODE_10_22_NPM %TOOLS%\.npm-6.14.6 /M
 setx NPM_HOME %NODE_10_NPM%
 setx NODE_HOME %NODE_10%
-setx MVN_HOME %TOOLS%\apache-maven-3.3.9 /M
+setx MVN_HOME %TOOLS%\apache-maven-3.6.0 /M
 
 setx GRADLE_4_7 %TOOLS%\gradle-4.7 /M
 setx GRADLE_5_6_4 %TOOLS%\gradle-5.6.4 /M
