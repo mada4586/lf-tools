49aad5c79c37937df1c15a66b72039f05ba70a28
Marcin Dabrowski <vgm@kmd.dk>
2020-10-17 15:52:05 +0200

    Running MySQL in docker-compose.

--- run-mysql.cmd
+++ run-mysql.cmd
@@ -1,2 +1,12 @@
-docker run -d -p 3307:3306 --name mysql_57 -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:5.7 --innodb_flush_log_at_trx_commit=2 --sql_mode=
+@echo off
+if [%1] == [up] set UPDOWN="up" -d
+if [%1] == [down] set UPDOWN="down"
 
+if [%1] == [] (
+    echo Usage: "%~n0 [up|down]"
+    goto :end
+)
+
+docker-compose -f %~dp0\mysql-apiserver-empty.yml %UPDOWN%
+
+:end
