05e5a41b43faf59621587df874b3441936ef0e75
Marcin <marcin.dabrowski@torque-its.com>
2022-01-13 12:15:20 +0100

    Removed some LF specific scripts. Added one for SQL Server.

--- reset-unity.cmd
+++
@@ -1,4 +0,0 @@
-mysqladmin -h 127.0.0.1 -u root -f drop unity
-mysqladmin -h 127.0.0.1 -u root create unity
-mysql -h 127.0.0.1 -u root unity < %1
-mysql -h 127.0.0.1 -u root unity < %~dp0\reset-unity.sql
---
+++ sql-server-start.cmd
@@ -0,0 +1,1 @@
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Test1234" -p 1433:1433 -v c:\Users\mada\Downloads:/mnt/Downloads --name sql2 -h sql2 -d mcr.microsoft.com/mssql/server:2019-latest
